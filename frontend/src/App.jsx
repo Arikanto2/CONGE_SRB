f70a9ac7bda13a85e3c38208529290c28b793fc0
Arikanto2 <fahazavanaandriantsoa@gmail.com>
2025-09-22 10:22:31 +0300

    Configuration des fichier et installation tailwind

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,11 @@
-
-import './App.css'
+import "./App.css";
 
 function App() {
-  
-
   return (
     <>
-      
+      <h1 className="text-left ">Hello Tailwind 🚀</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
