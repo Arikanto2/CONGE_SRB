73119890660a6cb8a642ae08c7621a2ce48b380b
PrincyRazafy <razafy805@gmail.com>
2025-09-25 10:04:06 +0300

    modif

--- frontend/src/Composants/App.jsx
+++ frontend/src/Composants/App.jsx
@@ -1,16 +1,18 @@
 import "../Style/App.css";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Login from "./Login.jsx";
+import Accueil from "../views/Accueil.jsx";
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Login></Login>} />
-          <Route path="/about" element={<h1>jj</h1>} />
+          <Route path="/" element={<Login></Login>}/>
+          <Route path="/about" element={<Accueil/>} />
         </Routes>
       </BrowserRouter>
+
     </>
   );
 }
